8808524757d5eec6ff6c6e54a28ee1f83153692b
BlackSwan-Decoded <aronaws@hotmail.com>
2020-10-24 16:12:47 -0400

    set minZoom

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ var mymap = L.map('mapid').setView([39.907, -75.031], 8);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    center: [39.056882, -98.407468], 
     maxZoom: 18,
+    minZoom: 5, 
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
