75447d1a45dd1df0dc2770d0a882f4e3ca9e5cd1
Frosty <joeyfrost007@gmail.com>
2020-10-28 13:43:10 -0400

    Added documentation to timeControlButtons.js

--- src/timeControlButtons.js
+++ src/timeControlButtons.js
@@ -2,9 +2,12 @@
  * JS code to control the function of the HTML buttons.
  */
 
-/* HTML Button functions */
 var paused = false;
 
+/**
+ * The play button's function. Makes the Covid data start moving
+ * forward in time starting with the current day.
+ */
 function pressPlay() {
 
     paused = false;
@@ -22,11 +25,19 @@ function pressPlay() {
     delay()
 }
 
+/**
+ * The pause button's function. Makes the Covid data stop moving.
+ * Stops time from moving forward.
+ */
 function pressPause() {
     paused = true;
     console.log(current_day);
 }
 
+/**
+ * The stop button's function. Stops the Covid data from moving and
+ * resets the current day back to the first day of the data.
+ */
 function pressStop() {
     pressPause();
     resetDay();
@@ -34,21 +45,41 @@ function pressStop() {
     covid.resetStyle();
 }
 
+/**
+ * Resets the current day to the first day in the covid data.
+ */
 async function resetDay() {
     current_day = 0;
 }
 
+/**
+ * Resets the view to the default view and position.
+ * Centered on the contiguous USA.
+ */
 function resetZoom() {
     mymap.setView([39.056882, -98.407468], 5);
 }
 
-function zoomToState(stateIndex) {
+/**
+ * Changes the view to the input state. Changes the zoom to the 
+ * input zoom.
+ * 
+ * @param {*} stateIndex The state being focused on.
+ * @param {*} zoomLevel The default zoom level for the state.
+ */
+function zoomToState(stateIndex, zoomLevel) {
     stateObject = State_Centroids.features[stateIndex];
     longitude = stateObject.properties.long;
     latitude = stateObject.properties.lat;
-    mymap.setView([latitude, longitude], 7);
+    mymap.setView([latitude, longitude], zoomLevel);
 }
 
+/**
+ * Sets the current state global variable to the state that is
+ * currently selected.
+ * 
+ * @param {*} stateName The new current state being selected.
+ */
 function setCurrentState(stateName) {
     current_state = stateName;
 }
