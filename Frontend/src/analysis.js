8c767718a3e7fff304d73f533ca5f265afbf075e
Jacelynn Duranceau <duranceaj3@students.rowan.edu>
2020-11-05 22:20:49 -0500

    modified the statistics functions

--- Frontend/src/analysis.js
+++ Frontend/src/analysis.js
@@ -2,47 +2,33 @@ function getStateAvgMedIncome() {
     var index = 0;
     var allFeatures = geoJson.features;    //array of all county features
     var length = allFeatures.length
-    var incomeArray = [];
+    var income = 0;
     var counter = 0;    //total counties of current state
     for( ; index < length ; index++)
     {
         var thisProperty = allFeatures[index].properties;    //this current county's properties
         if(thisProperty.STATE_NAME == current_state)
         {
-            incomeArray[counter] = thisProperty.Med_Income;  //add this county's median income to income array
+            income += thisProperty.Med_Income; 
             counter++;
         }
     }
-    index = 0;
-    var totalMed = 0;
-    for( ; index < counter ; index++)
-    {
-        totalMed += incomeArray[index];
-    }
-    totalMed = (totalMed/counter);
-    return totalMed;
+    income = (income/counter);
+    return income;
 }
 
 function getSingleCovid() {
     var index = 0;
     var allFeatures = geoJson.features;    //array of all county features
     var length = allFeatures.length
-    var covidArray = [];
-    var counter = 0;    //total counties of current state
+    var covidCases = 0;
     for( ; index < length ; index++)
     {
         var thisProperty = allFeatures[index].properties;    //this current county's properties
         if(thisProperty.STATE_NAME == current_state)
         {
-            covidArray[counter] = thisProperty.Covid_Cases[current_day];  //add this county's covid single day total to income array
-            counter++;
+            covidCases += thisProperty.Covid_Cases;  
         }
     }
-    index = 0;
-    var totalCases = 0;
-    for( ; index < counter ; index++)
-    {
-        totalCases += covidArray[index];
-    }
-    return totalCases;
+    return covidCases;
 }
